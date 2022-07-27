f01467959333d6b7e7dc230e2113070f7fd75602
Вадим Фатеев <fateevdanon@gmail.com>
2022-07-27 18:37:27 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import PhonebookForm from './Phonebook/PhonebookForm';
+import React, { Component } from 'react';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  onSubmitForm = e => {
+    e.preventDefault();
+    const { name, number, contacts } = this.state;
+    const contact = {
+      name,
+      number,
+    };
+    contacts.push(contact);
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <PhonebookForm
+          handleChange={this.handleChange}
+          onSubmitForm={this.onSubmitForm}
+        />
+      </div>
+    );
+  }
+}
--- src/components/Phonebook/PhonebookForm.jsx
+++ src/components/Phonebook/PhonebookForm.jsx
@@ -1,7 +1,7 @@
-function PhonebookForm({ addContact }) {
+export default function PhonebookForm({ handleChange, onSubmitForm }) {
   return (
-    <form>
-      <label htmlFor="">
+    <form onSubmit={onSubmitForm}>
+      <label>
         Name
         <input
           type="text"
@@ -9,6 +9,7 @@ function PhonebookForm({ addContact }) {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          onChange={handleChange}
         />
       </label>
       <label>
@@ -19,6 +20,7 @@ function PhonebookForm({ addContact }) {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          onChange={handleChange}
         />
       </label>
       <button type="submit">Add contact</button>
